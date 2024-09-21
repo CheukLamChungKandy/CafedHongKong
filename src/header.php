dcc56c2de719a32b41a4a10cdd28d41fbc49c50a
CheukLamChungKandy <cheuklamchungwork@gmail.com>
2024-09-22 03:52:13 +1000

    fix website empty problem

--- src/header.php
+++ src/header.php
@@ -1,5 +1,6 @@
 <head>
     <base href="https://www.cafedhongkong.com.au/CafedHongKong/">
+    <!-- <base href="http://localhost:8888/ "> -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Comatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,6 +11,7 @@
         rel="stylesheet" 
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
     >
+    
     <link
     rel="stylesheet"
     href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
